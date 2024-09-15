49c2ae8c69d31d645db1bfc50fbd971d24aa03b8
Joel Oliver Mclean <joelolivermclean@gmail.com>
2024-09-15 21:47:41 +0100

    Proxy?

--- src/helpers/NetworkHelper.js
+++ src/helpers/NetworkHelper.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: (import.meta.env.VITE_API_PREFIX ?? "") + "/api/",
+  baseURL: "/api/",
   withCredentials: true,
   headers: {
     "Content-type": "application/json",
