257079e876456ac203b8765872932fdddf3b32c8
Joel Oliver Mclean <joelolivermclean@gmail.com>
2024-09-16 09:31:10 +0100

    Fix home page padding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,10 +45,10 @@ const Home = () => {
             }
           : {}
       }
-      className={`flex flex-1 flex-col items-center justify-start gap-5 p-10 sm:justify-center sm:p-4`}
+      className={`flex flex-1 flex-col items-center justify-start gap-5 p-4 sm:justify-center sm:p-4`}
     >
       {loggedInUser ? (
-        <div className="flex w-full flex-1 flex-col gap-4 p-4 duration-200">
+        <div className="flex w-full flex-1 flex-col gap-4 duration-200">
           <h2 className="text-3xl">
             My<span className="font-semibold text-green-500">Groups</span>
           </h2>
@@ -88,25 +88,27 @@ const Home = () => {
           </div>
         </div>
       ) : (
-        <div
-          id="getStarted"
-          className="w-full rounded-xl bg-[#121212] bg-opacity-70 p-4 shadow-lg shadow-black backdrop-blur-sm sm:w-auto"
-        >
-          <div className="pb-3 text-7xl">
-            <h1 className="">Lets</h1>
-            <h1 className="font-semibold text-green-500">KickOff</h1>
-          </div>
-          <Registration registerNewUser={registerNewUser} />
-          <div className="flex flex-col items-center pt-3">
-            <Link
-              to={"/login"}
-              className="underline-offset-3 p-2 text-base hover:underline"
-            >
-              Already registered?{" "}
-              <span className="font-semibold text-green-500 hover:underline">
-                Login
-              </span>
-            </Link>
+        <div className="w-full p-6 sm:w-auto">
+          <div
+            id="getStarted"
+            className="w-full rounded-xl bg-[#121212] bg-opacity-70 p-4 shadow-lg shadow-black backdrop-blur-sm sm:w-auto"
+          >
+            <div className="pb-3 text-7xl">
+              <h1 className="">Lets</h1>
+              <h1 className="font-semibold text-green-500">KickOff</h1>
+            </div>
+            <Registration registerNewUser={registerNewUser} />
+            <div className="flex flex-col items-center pt-3">
+              <Link
+                to={"/login"}
+                className="underline-offset-3 p-2 text-base hover:underline"
+              >
+                Already registered?{" "}
+                <span className="font-semibold text-green-500 hover:underline">
+                  Login
+                </span>
+              </Link>
+            </div>
           </div>
         </div>
       )}
