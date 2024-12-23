704f72c1948200c44225a091bfbcde1f0a839cd7
Joel Oliver Mclean <joelolivermclean@gmail.com>
2024-12-23 13:46:46 +0000

    Add ratings dialog for Tom

--- src/pages/TeamPicker.jsx
+++ src/pages/TeamPicker.jsx
@@ -7,6 +7,12 @@ import TeamsPitchGraphic from "../components/TeamsPitchGraphic";
 import { toPng } from "html-to-image";
 import { toast } from "react-toastify";
 import moment from "moment";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
 
 export default function TeamPicker() {
   const { uuid } = useParams();
@@ -19,6 +25,7 @@ export default function TeamPicker() {
   const [selectedPlayers, setSelectedPlayers] = useState([]);
   const [teams, setTeams] = useState(null);
   const [generating, setGenerating] = useState(false);
+  const [showDialog, setShowDialog] = useState(false);
   // const [method, setMethod] = useState(methodChoice);
 
   const teamsGraphicRef = useRef(null);
@@ -75,7 +82,6 @@ export default function TeamPicker() {
   };
 
   const copyTeamsText = useCallback(() => {
-    console.log("boop");
     var text = `👕 Shirts 👕\n${teams?.team1
       .map((p, index) => `${index + 1}. ${p.name}`)
       .join("\n")}\n\n🦺 Bibs 🦺\n${teams?.team2
@@ -160,6 +166,105 @@ export default function TeamPicker() {
     });
   }, []);
 
+  const teamsInfoModal = (
+    <Dialog open={showDialog} onClose={setShowDialog} className="relative z-10">
+      <DialogBackdrop
+        transition
+        className="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+      />
+
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <DialogPanel
+            transition
+            className="relative w-full transform overflow-hidden rounded-lg border-solid bg-gradient-to-b from-slate-800 to-[#121212] px-3 py-4 text-left shadow-xl ring-2 ring-green-500 transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 sm:pb-4 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+          >
+            <div className="">
+              <div className="sm:flex sm:items-start">
+                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                  <DialogTitle
+                    as="h3"
+                    className="text-xl font-semibold leading-6 text-green-500"
+                  >
+                    Team Information
+                  </DialogTitle>
+                  <div className="mt-2 flex justify-center">
+                    <table className="w-[75%]">
+                      <tr>
+                        <td className="text-start">Shirts Avg. Rating</td>
+                        <td className="text-end">
+                          {teams?.team1?.reduce((acc, player, index, array) => {
+                            acc += player.rating;
+                            if (index === array.length - 1) {
+                              return acc / array.length;
+                            }
+                            return acc;
+                          }, 0)}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="text-start">Bibs Avg. Rating</td>
+                        <td className="text-end">
+                          {teams?.team2?.reduce((acc, player, index, array) => {
+                            acc += player.rating;
+                            if (index === array.length - 1) {
+                              return acc / array.length;
+                            }
+                            return acc;
+                          }, 0)}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="text-start">Shirts Total Rating</td>
+                        <td className="text-end">
+                          {teams?.team1?.reduce((acc, player, index, array) => {
+                            acc += player.rating;
+                            return acc;
+                          }, 0)}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="text-start">Bibs Total Rating</td>
+                        <td className="text-end">
+                          {teams?.team2?.reduce((acc, player, index, array) => {
+                            acc += player.rating;
+                            return acc;
+                          }, 0)}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="text-start">Avg. Rating Diff:</td>
+                        <td className="text-end">{teams?.avgDiff}</td>
+                      </tr>
+                      <tr>
+                        <td className="text-start">Total Rating Diff:</td>
+                        <td className="text-end">{teams?.totalDiff}</td>
+                      </tr>
+                      <tr>
+                        <td className="text-start">Goalie Diff:</td>
+                        <td className="text-end">{teams?.gkDiff}</td>
+                      </tr>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+              <button
+                type="button"
+                data-autofocus
+                onClick={() => setShowDialog(false)}
+                className="mt-3 inline-flex w-full justify-center rounded-md bg-slate-700 px-3 py-2 text-sm font-semibold shadow-sm ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+              >
+                OK
+              </button>
+            </div>
+          </DialogPanel>
+        </div>
+      </div>
+    </Dialog>
+  );
+
   return (
     <div className="px-4">
       {loading ? (
@@ -179,10 +284,18 @@ export default function TeamPicker() {
           >
             <i className="fa-solid fa-chevron-left"></i> {group?.name}
           </Link>
-          <div>
+          <div className="flex items-end justify-between">
             <h2 className="text-3xl">
               Team<span className="font-semibold text-green-500">Picker</span>
             </h2>
+            {teams && step === "teams" && (
+              <div
+                className="text-xl text-green-500"
+                onClick={() => setShowDialog(true)}
+              >
+                <i className="fa-solid fa-circle-info"></i>
+              </div>
+            )}
           </div>
           <div className="relative">
             {step === "players" && (
@@ -334,6 +447,7 @@ export default function TeamPicker() {
           </div>
         </div>
       )}
+      {teamsInfoModal}
     </div>
   );
 }
